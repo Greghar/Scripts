990bf8bf55134a4a526fd6cde7ad1c5063bdf83f
Greghar <gharlan8455@charter.net>
2015-08-19 23:02:59 -0500

    python 3.4.2
    install python

---
+++ scripts.py
@@ -0,0 +1,15 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:15:05) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=1,2,3,
+>>> ================================ RESTART ================================
+>>> 
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> (script)
+d = run
+c = module
+b = exe
+a = display
+  
+  
